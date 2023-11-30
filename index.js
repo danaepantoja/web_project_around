034f881c9dd8a690164455b6d3ce6f546dc0ea2b
Danae Pantoja <danae2910@outlook.es>
2023-11-29 23:30:26 -0600

    Java 1

--- index.js
+++ index.js
@@ -4,6 +4,9 @@ const closeButton = popupProfile.querySelector('.popup__button-close')
 
 const profileName = document.querySelector('.profile__name');
 const profileJob = document.querySelector('.profile__job');
+const formProfile =popupProfile.querySelector('.popup__form')
+const inputName = popupProfile.querySelector('form__input[name="name"]');
+const inputJob = popupProfile.querySelector('form__input[name="job"]');
 
 function togglePopup(popup){
     popupProfile.classList.toggle('popup_show')
@@ -16,3 +19,11 @@ buttonEdit.addEventListener('click', function(){
 closeButton.addEventListener('click', function(){
     togglePopup(popupProfile);
 });
+
+formProfile.addEventListener('submit', function(event){
+    event.preventDefault();
+    profileName.textContent = inputName.value;
+    profileJob.textContent = inputJob.value;
+    formProfile.reset();
+    togglePopup(popupProfile);
+})
