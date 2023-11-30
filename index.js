9aadb400c8db7612a12e983b6d6f55c33df93125
Danae Pantoja <danae2910@outlook.es>
2023-11-29 23:08:58 -0600

    Ventana nueva

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ const buttonEdit = document.querySelector('.edit__button')
 const popupProfile = document.querySelector('.popup_content_edit-profile')
 const closeButton = popupProfile.querySelector('.popup__button-close')
 
+const profileName = document.querySelector('.profile__name');
+const profileJob = document.querySelector('.profile__job');
+
 function togglePopup(popup){
     popupProfile.classList.toggle('popup_show')
 }
