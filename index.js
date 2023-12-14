1a5258f7a3c3d13adecf8b807787dc30cafaa786
Danae Pantoja <danae2910@outlook.es>
2023-12-14 10:31:10 -0600

    creacion imagenes en js

--- index.js
+++ index.js
@@ -1,3 +1,45 @@
+const cards = [
+    {
+      name: "Valle de Yosemite",
+      link: "https://practicum-content.s3.us-west-1.amazonaws.com/new-markets/WEB_sprint_5/ES/yosemite.jpg"
+    },
+    {
+      name: "Lago Louise",
+      link: "https://practicum-content.s3.us-west-1.amazonaws.com/new-markets/WEB_sprint_5/ES/lake-louise.jpg"
+    },
+    {
+      name: "Montañas Calvas",
+      link: "https://practicum-content.s3.us-west-1.amazonaws.com/new-markets/WEB_sprint_5/ES/bald-mountains.jpg"
+    },
+    {
+      name: "Latemar",
+      link: "https://practicum-content.s3.us-west-1.amazonaws.com/new-markets/WEB_sprint_5/ES/latemar.jpg"
+    },
+    {
+      name: "Parque Nacional de la Vanoise",
+      link: "https://practicum-content.s3.us-west-1.amazonaws.com/new-markets/WEB_sprint_5/ES/vanoise.jpg"
+    },
+    {
+      name: "Lago di Braies",
+      link: "https://practicum-content.s3.us-west-1.amazonaws.com/new-markets/WEB_sprint_5/ES/lago.jpg"
+    }
+  ];
+  
+const container = document.querySelector(".elements");
+cards.forEach(function (item) {
+
+const template = document.querySelector("#element-template").content;
+const element = template.querySelector(".element").cloneNode(true);
+
+const imageElement = element.querySelector(".element__image");
+const titleElement = element.querySelector(".element__title");
+
+imageElement.src = item.link
+titleElement.innerText = item.name
+
+container.append(element);
+})
+  
 const buttonEdit = document.querySelector('.edit__button')
 const popupProfile = document.querySelector('.popup_content_edit-profile')
 const closeButton = popupProfile.querySelector('.popup__button-close')
